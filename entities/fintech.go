991213eefb35a7636f1a4576f09ee42934c16680
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2023-02-10 19:53:07 -0300

    feat: add crud to fintech resource

--- entities/fintech.go
+++ entities/fintech.go
@@ -1,10 +1,9 @@
 package entities
 
 type Fintech struct {
+	Id           int    `json:"id"`
 	Name         string `json:"name"`
 	Document     string `json:"document"`
 	IconUrl      string `json:"iconUrl"`
 	SocialReason string `json:"socialReason"`
 }
-
-var Fintechs []Fintech
--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"go-fintechs-api/entities"
+	"go-fintechs-api/database"
 	"go-fintechs-api/routes"
 )
 
 func main() {
-	entities.Fintechs = []entities.Fintech{{Name: "Nubank", Document: "18.236.120/0001-58", IconUrl: "https://commons.wikimedia.org/wiki/File:Nubank_logo_2021.svg#/media/File:Nubank_logo_2021.svg", SocialReason: "NU FINANCEIRA S.A. – SOCIEDADE DE CRÉDITO, FINANCIAMENTO E INVESTIMENTO"}}
+	database.DatabaseConnect()
 	fmt.Println("init go server")
 	routes.HandleRequest()
 }
