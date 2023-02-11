e97d297cbd11b8e2a5b757079f6bd7e1cf219767
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2023-02-10 21:20:39 -0300

    feat: read .env database variables

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"go-fintechs-api/database"
+	"go-fintechs-api/env"
 	"go-fintechs-api/routes"
 )
 
 func main() {
-	database.DatabaseConnect()
+	host := env.GoDotEnvVariable("DB_HOST")
+	user := env.GoDotEnvVariable("DB_USER")
+	password := env.GoDotEnvVariable("DB_PASSWORD")
+	dbName := env.GoDotEnvVariable("DB_NAME")
+	port := env.GoDotEnvVariable("DB_PORT")
+	database.DatabaseConnect(host, user, dbName, password, port)
 	fmt.Println("init go server")
 	routes.HandleRequest()
 }
