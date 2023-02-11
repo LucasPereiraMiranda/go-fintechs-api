c8f865dfbc3680b6b541a332a51e23d4088fda7f
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2023-02-10 21:33:58 -0300

    refactor: change dir and package name to config

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
+	"go-fintechs-api/config"
 	"go-fintechs-api/database"
-	"go-fintechs-api/env"
 	"go-fintechs-api/routes"
 )
 
 func main() {
-	host := env.GoDotEnvVariable("DB_HOST")
-	user := env.GoDotEnvVariable("DB_USER")
-	password := env.GoDotEnvVariable("DB_PASSWORD")
-	dbName := env.GoDotEnvVariable("DB_NAME")
-	port := env.GoDotEnvVariable("DB_PORT")
+	host := config.GoDotEnvVariable("DB_HOST")
+	user := config.GoDotEnvVariable("DB_USER")
+	password := config.GoDotEnvVariable("DB_PASSWORD")
+	dbName := config.GoDotEnvVariable("DB_NAME")
+	port := config.GoDotEnvVariable("DB_PORT")
+
 	database.DatabaseConnect(host, user, dbName, password, port)
 	fmt.Println("init go server")
 	routes.HandleRequest()
